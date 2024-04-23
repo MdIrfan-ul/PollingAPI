faa55343ba73a1624e4ce2dc7de3dfd9b3681c03
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-04-24 00:43:03 +0530

    db connection added

--- src/config/mongoose.db.js
+++ src/config/mongoose.db.js
@@ -5,7 +5,7 @@ const dbConnection = async ()=>{
     try {
         console.log("connecting to db-->");
     const resp =await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`connected to: ${resp.connection.host}`);
+    // console.log(`connected to: ${resp.connection.host}`);
     } catch (error) {
         console.log("Something went wrong while connecting to DataBase:",error);
     console.log(error);
